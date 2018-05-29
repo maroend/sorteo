cf1bbfb850ee77560e114f28103baebadb4af9f6
Charly Ruiz García <soycharly@live.com>
2018-05-28 19:00:52 -0500

    Cambio para trabajar en local

--- WebContent/assets/js/SSOffice365.js
+++ WebContent/assets/js/SSOffice365.js
@@ -32,7 +32,7 @@ var ADAL = new AuthenticationContext({
 	instance: 'https://login.microsoftonline.com/',
 	tenant: 'common', //COMMON OR YOUR TENANT ID
 
-	/* produccion */
+	/* produccion 
 	clientId: '83f48914-35de-472c-a243-0f987a169e2a', //REPLACE WITH YOUR CLIENT ID PRODUCCION
 	redirectUri: 'https://sorteoanahuac-sistema.azurewebsites.net/sorteo/ssoOffice365.html',//REPLACE WITH YOUR REDIRECT URL PRODUCCION
 	/**/
@@ -43,7 +43,7 @@ var ADAL = new AuthenticationContext({
 
 	/**/
 	
-	/* local *
+	/* local */
 	clientId: 'b8e607c3-641d-4488-b75a-e6390cd3fed1',
 	redirectUri: 'http://localhost:8080/sorteo/ssoOffice365.html',
 	/**/
